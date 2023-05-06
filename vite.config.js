c0b4b0f2a1ca5c40508885b5a566fc0502519dd3
rick <Rick.Hou@aspiraconnect.com>
2023-05-06 14:54:53 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -1,57 +1,54 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import { VitePWA } from 'vite-plugin-pwa'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 
 const pwaOptions = {
-  injectRegister: 'inline',
-  registerType: 'prompt',
-  mode: 'development',
-  base: '/',
-  includeAssets: ['favicon.svg'],
+  injectRegister: "inline",
+  registerType: "prompt",
+  mode: "development",
+  base: "/",
+  includeAssets: ["vite.svg", "react.svg"],
   manifest: {
-    name: 'PWA test',
-    short_name: 'PWA test',
-    theme_color: '#ffffff',
+    name: "PWA test",
+    short_name: "PWA test",
+    theme_color: "#ffffff",
     icons: [
       {
-        src: 'pwa-192x192.png', // <== don't add slash, for testing
-        sizes: '192x192',
-        type: 'image/png',
+        src: "logo192.png", // <== don't add slash, for testing
+        sizes: "192x192",
+        type: "image/png",
       },
       {
-        src: '/pwa-512x512.png', // <== don't remove slash, for testing
-        sizes: '512x512',
-        type: 'image/png',
+        src: "/logo512.png", // <== don't remove slash, for testing
+        sizes: "512x512",
+        type: "image/png",
       },
       {
-        src: 'pwa-512x512.png', // <== don't add slash, for testing
-        sizes: '512x512',
-        type: 'image/png',
-        purpose: 'any maskable',
+        src: "logo512.png", // <== don't add slash, for testing
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any maskable",
       },
     ],
   },
   devOptions: {
     enabled: true,
     /* when using generateSW the PWA plugin will switch to classic */
-    type: 'module',
-    navigateFallback: 'index.html',
+    type: "module",
+    navigateFallback: "index.html",
   },
-  strategies:'injectManifest',
-  filename:'claims-sw.ts',
-  srcDir:'src',
+  strategies: "injectManifest", // custom service worker
+  filename: "claims-sw.js",
+  srcDir: "src",
   workbox: {
-    sourcemap: true
-  }
-}
+    sourcemap: true,
+  },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port:5174
+    port: 5174,
   },
-  plugins: [
-    react(),
-    VitePWA(pwaOptions),
-  ],
-})
+  plugins: [react(), VitePWA(pwaOptions)],
+});
